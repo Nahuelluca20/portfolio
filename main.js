771f73b5bbcdfa9adde4587768aaabf7d3505c5d
Nahuelluca20 <nahuelluca@gmail.com>
2021-09-05 16:17:50 -0300

    menu fiz

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
-const menu = document.getElementById('menu-list')
-const btMenu = document.getElementById('menu-bt')
-const navbar = document.querySelector('nav')
+const menu = document.getElementById("menu-list");
+const btMenu = document.getElementById("menu-bt");
+const navbar = document.querySelector("nav");
 
 window.addEventListener("scroll", function () {
   window.scrollY > 100 && (navbar.style.background = `rgb(239, 83, 80, 0.9)`);
   window.scrollY < 100 && (navbar.style.background = `transparent`);
 });
 
-btMenu.addEventListener('click', function() {
-  if(menu.style.display === 'none') {
-    menu.style.display = 'block'
+btMenu.addEventListener("click", function () {
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
   } else {
-    menu.style.display = 'none'
+    menu.style.display = "none";
   }
-})
-
+});
 
 function hover(x) {
-  x.childNodes[1].style.display = 'block'
+  x.childNodes[1].style.display = "block";
 }
 
 function outHover(x) {
-  x.childNodes[1].style.display = 'none'
+  x.childNodes[1].style.display = "none";
 }
 
+
+[...document.querySelectorAll('.menu-item')].forEach(function(item) {
+  item.addEventListener('click', function() {
+    menu.style.display = "none"
+  });
+});
